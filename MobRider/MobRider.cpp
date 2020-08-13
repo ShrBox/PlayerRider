b9af98162ebecc3fe31b69b36f47966274912f73
ShrBox <shrbox@outlook.com>
2020-08-13 13:35:40 +0800

    bugfix

--- MobRider/MobRider.cpp
+++ MobRider/MobRider.cpp
@@ -2,11 +2,22 @@
 
 void entry()
 {
-	std::cout << "[MobRider] Loaded!\n";
+	std::cout << "[PlayerRider] Loaded!\n";
 }
 
 THook(bool, "?attack@Player@@UEAA_NAEAVActor@@@Z", Player* pl, Actor* ac)
 {
+	if (pl->getCarriedItem().getId() == 271 && ac->getEntityTypeId() == 1)
+	{
+		SymCall("?_removeRider@Actor@@MEAAXAEBUActorUniqueID@@_N11@Z", void, Actor*, const ActorUniqueID*, bool, bool, bool)
+			(
+			ac, &pl->getUniqueID(), true, true, true
+			);
+		pl->kill();
+		auto pl2 = LocateS<WLevel>()->getPlayer(pl->getNameTag());
+		pl2.value().sendText(u8"已正常脱离");
+		return false;
+	}
 	if (pl->getCarriedItem().getId() == 270 && ac->getEntityTypeId() == 1)
 	{
 		auto ac2 = LocateS<WLevel>()->getPlayer(ac->getNameTag());
@@ -16,7 +27,7 @@ THook(bool, "?attack@Player@@UEAA_NAEAVActor@@@Z", Player* pl, Actor* ac)
 			return false;
 		}
 		ac2.value()->addRider(*pl);
-		pl2.value().sendText(u8"因为BUG，你需要骑上马并让被骑的玩家断开服务器才能真正地取消乘骑状态");
+		pl2.value().sendText(u8"因为BUG，你需要拿木斧点击对方才能真正地取消乘骑状态");
 		return false;
 	}
 	return original(pl, ac);
